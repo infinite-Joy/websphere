9a4f19795112aa571b823c7a22d307133f58419d
infinite-Joy <joydeepubuntu@gmail.com>
2017-01-06 16:58:02 +0530

    add code for modify session tracking

--- create_listener_port.py
+++ create_listener_port.py
@@ -39,3 +39,7 @@ def main():
     createMessageListener(nodeName, serverName, listenerName1, queueName,
         connFactory, "START", "10", "2")
     print "done"
+
+
+if __name__ == "__main__" or __name__ == "main":
+    main()
---
+++ modify_session_tracking.py
@@ -0,0 +1,26 @@
+import java.lang.System
+
+cellName = ""
+nodeName = ""
+serverName = ""
+cookieName = ""
+
+
+def modifySessionTracking(serverName, nodeName, cellName, cookieName):
+    print "Updating Session Tracking Settings for ..." + "Server: " + serverName
+        + " Cell: " + cellName + "Node:" + nodeName
+    server = AdminConfig.getid("/Cell:" + cellName + "/Node:" + nodeName +
+                             "/Server:" + serverName + "/")
+    session_manager = AdminConfig.list("SessionManager", server)
+    AdminConfig.modify(session_manager, [["enableCookies", "true"],
+        "defaultCookieSettings", [["name", cookieName], ["secure", "true"]]])
+    print "Saving Changes..."
+    AdminConfig.save()
+
+
+def main():
+    modifySessionTracking(serverName, nodeName, cellName, cookieName)
+
+
+if __name__ == "__main__" or __name__ == "main":
+    main()
