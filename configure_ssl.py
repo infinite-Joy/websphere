954437213fcf1f50a5da5ffcf60b66f919ad895e
infinite-Joy <joydeepubuntu@gmail.com>
2017-01-16 23:29:47 +0530

    configure ssl: first commit

---
+++ configure_ssl.py
@@ -0,0 +1,10 @@
+"""
+This script will configure the JVM SSL outbound certificate Configuration
+"""
+
+import sys
+import java
+global AdminConfig
+
+# set the JKS key store path
+JKSKeyStorePath = "/path/to/JKS"
