10c30aca723984e9e2169018050ddf97a26951b6
infinite-Joy <joydeepubuntu@gmail.com>
2017-01-19 09:26:37 +0530

    configure ssl: createKeyStore

--- configure_ssl.py
+++ configure_ssl.py
@@ -9,3 +9,15 @@ global AdminConfig
 # set the JKS key store path
 JKSKeyStorePath = "/path/to/JKS"
 keyPassword = ""
+
+def configureSSLConfiguration(cellName, nodeName, serverName, applicationName):
+    keystoreName = applicationName + "_TS_CONFIG"
+    print "Creating key store at the server level scope: " + keystoreName
+
+    AdminTask.createKeyStore("[ -scopeName (cell):" + cellName
+        + ":(node):" + nodeName + ":(server):" + serverName
+        + " -keyStoreName " + keyStoreName
+        + " - keyStoreType JKS - keyStoreLocation "
+        + JKSKeyStorePath + " -keyStorePassword " + keyPassword
+        + " -keyStorePasswordVerify " + keyPassword
+        + "keyStoreisFileBased true -keyStoreReadOnly false")
