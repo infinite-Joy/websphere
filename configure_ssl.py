4e42bb4a54cc4d1809119bdb57dcc2604fb9dece
infinite-Joy <joydeepubuntu@gmail.com>
2017-01-20 09:30:59 +0530

    finish configureSSLConfiguration

--- configure_ssl.py
+++ configure_ssl.py
@@ -14,10 +14,38 @@ def configureSSLConfiguration(cellName, nodeName, serverName, applicationName):
     keystoreName = applicationName + "_TS_CONFIG"
     print "Creating key store at the server level scope: " + keystoreName
 
-    AdminTask.createKeyStore("[ -scopeName (cell):" + cellName
+    cell_node_server_scope = "(cell):" + cellName\
         + ":(node):" + nodeName + ":(server):" + serverName
+
+    AdminTask.createKeyStore("[ -scopeName " + cell_node_server_scope
         + " -keyStoreName " + keyStoreName
         + " - keyStoreType JKS - keyStoreLocation "
         + JKSKeyStorePath + " -keyStorePassword " + keyPassword
         + " -keyStorePasswordVerify " + keyPassword
         + "keyStoreisFileBased true -keyStoreReadOnly false")
+
+    SSL_ConfigName = serverName + "_SSL_CONFIG"
+    AdminTask.createSSLConfigGroup("[ -alias " + SSL_ConfigName
+        + " -scopeName " + cell_node_server_scope + " -trustStoreName " + keyStoreName
+        + " -trustStoreScopeName " + cell_node_server_scope
+        + " -keyStoreName CellDefaultKeyStore -keyStoreScopeName (cell):"
+        + cellName + " ]")
+
+    print " Created SSL config .. " + SSL_ConfigName
+
+    AdminTask.createSSLConfigGroup("[-name " + serverName
+       + " -direction outbound -certificateAlias default -scopeName"
+       + cell_node_server_scope + " -sslConfigAliasName " + SSL_ConfigName
+       + " -sslConfigScopeName " + cell_node_server_scope + "]")
+
+    AdminConfig.save()
+
+    print " SSL configuration has been successfully configured for the server: " + serverName
+
+
+def main():
+    configureSSLConfiguration("cell", "node", "server", "app")
+
+
+if __name__ == "__main__" or __name__ == "main":
+    main()
