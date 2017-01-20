96f0c3058e974031cb9513ade2fbf2753a21f452
infinite-Joy <joydeepubuntu@gmail.com>
2017-01-20 21:34:46 +0530

    weblogic

---
+++ weblogic_sample.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+techpaste.com: simple wlst start stop script monitor check status management
+servers progress bar weblogic server
+/slot/oracle/weblogic/myDomain/wlserver_10.3/server/lib/weblogic.jar
+weblogic.wlst ms.py -u weblogic -nameOfServer -a hostserver:port -n $1 -c $2
+"""
+
+import sys
+import os
+from java.lang import System
+import getopt
+
+def getopt():
+    pass
